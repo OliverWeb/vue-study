813df27440d49ec3a94acdae227269ff31659e5c
zhangle <632231327@qq.com>
2019-04-23 06:58:53 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Antd from "ant-design-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "ant-design-vue/dist/antd.less";
+Vue.config.productionTip = false;
+Vue.use(Ant);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
